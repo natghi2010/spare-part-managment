704ca9351c30c2ae590611ec515e224a8ebf8910
Natnael Ghirma <natghi2010@gmail.com>
2021-01-09 22:29:18 +0300

    initial commit

--- spart-part/resources/views/welcome.blade.php
+++ spart-part/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello Heran
                 </div>
 
                 <div class="links">
