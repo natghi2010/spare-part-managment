6b5a2d32affc8eaa34c6ca24ecae738f6cd738be
Natnael Ghirma <natghi2010@gmail.com>
2021-01-13 23:20:16 +0300

    setup dashboard

---
+++ spart-part/resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+
+@section('content')
+
+<h2>Dashboard</h2>
+
+<p>Sales Report</p>
+<p>Supply List</p>
+<p>Low Stock</p>
+    
+@endsection
---
+++ spart-part/resources/views/layouts/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" crossorigin="anonymous" />
+    <title>Spart Part</title>
+</head>
+<body>
+ 
+
+<div class="container">
+@yield('content')
+</div>
+
+
+</body>
+</html>
