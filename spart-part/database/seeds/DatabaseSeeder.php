7f47313e936bd5a5a967d41a60d0e4297829e337
Natnael Ghirma <natghi2010@gmail.com>
2021-01-13 23:59:28 +0300

    added customer model

--- spart-part/database/seeds/DatabaseSeeder.php
+++ spart-part/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,12 +12,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        //creates fake user
         DB::table('users')->insert([
             'name' =>'Demo',
             'email' => 'demo@gmail.com',
             'password' => Hash::make('password'),
             'type' => 'Admin',
         ]);
+
+        //creates fake 10 customers
+      
+            $faker = Faker::create();
+        	foreach (range(1,100) as $index) {
+	        DB::table('customers')->insert([
+	            'name' => $faker->name,
+	            'email' => $faker->email,
+	            'phone' => rand(10000,40000),
+	            'tin_number' => $faker->numberBetween(10000,50000000)
+	        ]);
+	
+        }
+
         // $this->call(UsersTableSeeder::class);
     }
 }
--- spart-part/resources/views/dashboard.blade.php
+++ spart-part/resources/views/dashboard.blade.php
@@ -5,8 +5,26 @@
 
 <h2>Dashboard</h2>
 
-<p>Sales Report</p>
-<p>Supply List</p>
-<p>Low Stock</p>
-    
+<table class="table table-striped">
+    <thead>
+    <tr>
+        <th>Name </th>
+        <th>Email </th>
+        <th>Phone</th>
+        <th>Tin Number</th>
+    </tr>
+    </thead>
+    <tbody>
+        @foreach($customers as $customer)
+        <tr>
+            <td>{{$customer->name}}</td>
+            <td>{{$customer->email}}</td>
+            <td>{{$customer->phone}}</td>
+            <td>{{$customer->tin_number}}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+{{$customers->links()}}
 @endsection
