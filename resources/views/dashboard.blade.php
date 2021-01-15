91e142ad436a596812f02d2b6135457ba5794a9d
Natnael Ghirma <natghi2010@gmail.com>
2021-01-15 23:36:37 +0300

    setup UI scaffolding

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,30 +1,8 @@
 @extends('layouts.master')
 
+@section('title','Dashboard')
 
 @section('content')
 
-<h2>Dashboard</h2>
 
-<table class="table table-striped">
-    <thead>
-    <tr>
-        <th>Name </th>
-        <th>Email </th>
-        <th>Phone</th>
-        <th>Tin Number</th>
-    </tr>
-    </thead>
-    <tbody>
-        @foreach($customers as $customer)
-        <tr>
-            <td>{{$customer->name}}</td>
-            <td>{{$customer->email}}</td>
-            <td>{{$customer->phone}}</td>
-            <td>{{$customer->tin_number}}</td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
-
-{{$customers->links()}}
 @endsection
