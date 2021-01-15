95c489dd96511324853195f61f4cb6674d824e91
Natnael Ghirma <natghi2010@gmail.com>
2021-01-15 22:37:18 +0300

    minor fix

--- public/index.php
+++ public/index.php
@@ -9,6 +9,9 @@
 
 define('LARAVEL_START', microtime(true));
 
+
+$customPath = $_SERVER['HTTP_HOST'] == 'localhost:8000' ? '../' :'';
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
@@ -21,7 +24,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/'.$customPath.'vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +38,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/'.$customPath.'bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
