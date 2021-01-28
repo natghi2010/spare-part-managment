8278e66a2dcc05db0a4a59f105717a6a196ef6dc
heran <lyuyee123@gmail.com>
2021-01-28 03:53:59 +0300

    create supplier table

---
+++ database/migrations/2021_01_27_220546_create_suppliers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('country_of_origin');
+            $table->string('tin_number')->max(10)->unique();
+            $table->string('phone')->max(10)->unique();
+            $table->string('email')->unique();
+            $table->string('status')->default('Active');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
