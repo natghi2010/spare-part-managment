d7cdf496d51ddaef5680db66dbfabcd5ac0d3b2b
heran <lyuyee123@gmail.com>
2021-01-28 03:51:13 +0300

    minor change on the sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,6 +1,6 @@
     <!--  BEGIN SIDEBAR  -->
     <div class="sidebar-wrapper sidebar-theme">
-            
+
         <nav id="sidebar">
             <div class="profile-info">
                 <figure class="user-cover-image"></figure>
@@ -12,15 +12,15 @@
             </div>
             <div class="shadow-bottom"></div>
             <ul class="list-unstyled menu-categories" id="accordionExample">
-                <li class="menu">
-                    <a href="{{ route('dashboard')}} " aria-expanded="false" class="dropdown-toggle">
+                <li class="menu active">
+                    <a href="{{ route('dashboard')}}" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle">
                         <div class="">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
                             <span> Dashboard</span>
                         </div>
                     </a>
                 </li>
-                
+
                 <li class="menu">
                     <a href="{{ route('users')}} " aria-expanded="false" class="dropdown-toggle">
                         <div class="">
@@ -31,6 +31,15 @@
                 </li>
 
                 <li class="menu">
+                    <a href="{{ route('suppliers')}} " aria-expanded="false" class="dropdown-toggle">
+                        <div class="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                            <span>Suppliers</span>
+                        </div>
+                    </a>
+                </li>
+
+                {{-- <li class="menu">
                     <a href="#submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                         <div class="">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-airplay"><path d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"></path><polygon points="12 15 17 21 7 21 12 15"></polygon></svg>
@@ -46,11 +55,11 @@
                         </li>
                         <li>
                             <a href="javascript:void(0);"> Submenu 2 </a>
-                        </li>                           
+                        </li>
                     </ul>
-                </li>
+                </li> --}}
 
-                <li class="menu">
+                {{-- <li class="menu">
                     <a href="#submenu2" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                         <div class="">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
@@ -66,7 +75,7 @@
                         </li>
                         <li>
                             <a href="#sm2" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"> Submenu 2 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg> </a>
-                            <ul class="collapse list-unstyled sub-submenu" id="sm2" data-parent="#submenu2"> 
+                            <ul class="collapse list-unstyled sub-submenu" id="sm2" data-parent="#submenu2">
                                 <li>
                                     <a href="javascript:void(0);"> Sub-Submenu 1 </a>
                                 </li>
@@ -79,9 +88,9 @@
                             </ul>
                         </li>
                     </ul>
-                </li>
+                </li> --}}
 
-                <li class="menu active">
+                {{-- <li class="menu active">
                     <a href="#starter-kit" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle">
                         <div class="">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-terminal"><polyline points="4 17 10 11 4 5"></polyline><line x1="12" y1="19" x2="20" y2="19"></line></svg>
@@ -102,10 +111,10 @@
                             <a href="starter_kit_collapsible_menu.html"> Collapsible </a>
                         </li>
                     </ul>
-                </li>
-                
+                </li> --}}
+
             </ul>
-            
+
         </nav>
 
     </div>
