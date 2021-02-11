0e2b6c773318156a161cf44a90a82b1058c339f3
Natnael Ghirma <natghi2010@gmail.com>
2021-02-12 00:54:36 +0300

    added relations for parts

--- app/parts.php
+++ app/parts.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Vehicle;
+use App\PartType;
 
-class parts extends Model
+class Parts extends Model
 {
-    //
+    
+    public function vehicle(){
+        return $this->belongsTo(Vehicle::class,'vehicle_id');
+    }
+
+    public function part_type(){
+        return $this->belongsTo(PartType::class,'part_type_id');
+    }
+
 }
