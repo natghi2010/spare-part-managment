9040162d26673f625f36b8f478d173132917ecec
Natnael Ghirma <natghi2010@gmail.com>
2021-02-13 23:52:16 +0300

    added transactional info

---
+++ database/migrations/2021_02_13_201926_create_transaction_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+            $table->string('transaction_id')->nullable();
+            $table->string('type')->nullable();
+            $table->unsignedBigInteger('customer_id')->nullable();
+            $table->unsignedBigInteger('supplier_id')->nullable();
+            $table->unsignedBigInteger('part_id')->nullable();
+            $table->unsignedBigInteger('part_type_Id')->nullable();
+            $table->unsignedBigInteger('vehicle_id')->nullable();
+            $table->float('price')->nullable();
+            $table->bigInteger('qty')->nullable();
+            $table->bigInteger('new_balance')->nullable();
+            $table->bigInteger('date')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction');
+    }
+}
