20dd561e41be753f7be60c019b576d817a631643
NATNAEL GHIRMA <natghi2010@gmail.com>
2021-02-27 14:11:15 +0300

    fixed sidebar active page

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -22,9 +22,10 @@
                 </li>
 
                 <li class="menu">
-                    <a href="{{ route('users')}} " aria-expanded="{{'user' == request()->path() ? 'true' : 'false' }}" class="dropdown-toggle">
+                    <a href="{{ route('users')}} " aria-expanded="{{Request::is('user/*') ? 'true' : 'false' }}" class="dropdown-toggle">
                         <div class="">
-                            <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg> <span> Users</span>
+                            <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg> 
+                            <span> Users</span>
                         </div>
                     </a>
                 </li>
