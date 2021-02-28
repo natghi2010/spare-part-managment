20198664bd0b2402bd38b075ced1f666a2fa2396
NATNAEL GHIRMA <natghi2010@gmail.com>
2021-02-28 20:46:10 +0300

    minor change

--- resources/views/view-customer.blade.php
+++ resources/views/view-customer.blade.php
@@ -7,7 +7,7 @@
 <div class="col-xl-4 col-sm-offset-3 col-lg-6 col-md-6 col-sm-6 layout-spacing">
     <div class="widget-two">
         <div class="widget-content" style="min-height: 300px; ">
-            
+            <h1>Customer</h1>
         </div>
     </div>
 </div>
