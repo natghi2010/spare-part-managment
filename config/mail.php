1c5e3b819d9f9e52755338595c6df89049ff7913
NATNAEL GHIRMA <natghi2010@gmail.com>
2021-03-07 14:56:37 +0300

    added mail fix

--- config/mail.php
+++ config/mail.php
@@ -74,9 +74,10 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'natnael@jasmineaddis.com'),
+        'name' => env('MAIL_FROM_NAME', 'Natnael'),
     ],
+
 
     /*
     |--------------------------------------------------------------------------
