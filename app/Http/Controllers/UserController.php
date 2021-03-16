05f3144e5f259d42ead41ee692b66ea2f4b8493e
NATNAEL GHIRMA <natghi2010@gmail.com>
2021-03-16 23:01:30 +0300

    minor fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,28 +13,32 @@ class UserController extends Controller
 {
 
     //display all users
-    public function index(){
+    public function index()
+    {
         $users = User::all();
-        return view('list-users',['users'=>$users]);
+        return view('list-users', ['users' => $users]);
     }
 
-    public function create(){
+    public function create()
+    {
         return view('create-edit-user');
     }
 
-    public function edit($id){
+    public function edit($id)
+    {
         $user = User::find($id);
-        return view('create-edit-user',['user'=>$user]);
+        return view('create-edit-user', ['user' => $user]);
     }
 
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
 
-        $this->validate($request,[
-            'name'=>'required|max:50',
-            'email'=>'required|unique:users',
-            'type'=>'required'
-         ]);
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|unique:users',
+            'type' => 'required'
+        ]);
 
         $user = new User;
         $user->name = $request->name;
@@ -48,88 +52,88 @@ class UserController extends Controller
         $activity_log->user_id = auth()->user()->id;
         $activity_log->action = 'Added User';
         $activity_log->save();
-         //Logging Activity END
+        //Logging Activity END
 
-        return redirect(route('users'))->with('mssg','Successfully created a User');
+        return redirect(route('users'))->with('mssg', 'Successfully created a User');
     }
 
-    public function update(Request $request){
+    public function update(Request $request)
+    {
         $user = User::find($request->id);
         $user->name = $request->name;
         $user->email = $request->email;
         $user->type = $request->type;
         $user->save();
 
-        return redirect(route('edit-user',['id'=>$request->id]))->with('mssg','Successfully Update User');
+        return redirect(route('edit-user', ['id' => $request->id]))->with('mssg', 'Successfully Update User');
     }
 
 
-    public function trash($id){
+    public function trash($id)
+    {
         $user = User::find($id);
-        if($user->delete()){
-            return "Successfully Deleted User - ".Str::upper($user->name);
+        if ($user->delete()) {
+            return "Successfully Deleted User - " . Str::upper($user->name);
         }
     }
 
-    public function restore($id){
+    public function restore($id)
+    {
         $user = User::find($id);
-        if($user->restore()){
-            return "Successfully Restored User - ".Str::upper($user->name);
+        if ($user->restore()) {
+            return "Successfully Restored User - " . Str::upper($user->name);
         }
     }
 
 
-
-
-    public function showUsersActivity(){
+    public function showUsersActivity()
+    {
         $activity_logs = ActivityLog::with('user')->get();
-        return view('activity-log',['activity_logs'=>$activity_logs]);
+        return view('activity-log', ['activity_logs' => $activity_logs]);
     }
-    public function showMyActivity(){
+    public function showMyActivity()
+    {
 
-       $user =  User::where('id',auth()->user()->id)->with('activity_logs')->get()->first();
-       return view('activity-log',['activity_logs'=>$user->activity_logs]);
-
+        $user =  User::where('id', auth()->user()->id)->with('activity_logs')->get()->first();
+        return view('activity-log', ['activity_logs' => $user->activity_logs]);
     }
 
 
 
-    public function createTimeStampAlias(){
+    public function createTimeStampAlias()
+    {
         $activity_logs = ActivityLog::all();
         $now = Carbon::now();
-        foreach($activity_logs as $log){
+        foreach ($activity_logs as $log) {
             $log->duration = $log->created_at->diffForHumans($now);
         }
-        $sorted = $activity_logs->sortBy('duration')->skip(0)->take(6);
-        return view('dashboard',['activity_logs'=>$sorted]);
-
+        $activity_logs = $activity_logs->sortBy('duration')->skip(0)->take(6);
+        return view('dashboard', ['activity_logs' => $activity_logs]);
     }
 
 
-    public function profile(){
+    public function profile()
+    {
         return view('profile');
     }
 
-    public function changePassword(Request $request){
+    public function changePassword(Request $request)
+    {
 
         $user = User::find(auth()->user()->id);
 
-        if(Hash::check($request->current_password, $user->password)){
-           if($request->new_password == $request->confirm_password){
+        if (Hash::check($request->current_password, $user->password)) {
+            if ($request->new_password == $request->confirm_password) {
 
 
-                $user->update(['password'=>hash::make($request->new_password)]);
+                $user->update(['password' => hash::make($request->new_password)]);
 
-                return redirect(route('profile'))->with('mssg','Password successfully Changed');
-
-           }else{
-            return redirect(route('profile'))->with('err','Your new password must match your confirm password');
-           }
-        }else{
-            return redirect(route('profile'))->with('err','Your current password does not match our records');
+                return redirect(route('profile'))->with('mssg', 'Password successfully Changed');
+            } else {
+                return redirect(route('profile'))->with('err', 'Your new password must match your confirm password');
+            }
+        } else {
+            return redirect(route('profile'))->with('err', 'Your current password does not match our records');
         }
-
-
     }
-
 }
