e2bde380dcfa680f12d059a6facc8a91d3c58907
NATNAEL GHIRMA <natghi2010@gmail.com>
2021-03-23 23:12:07 +0300

    minor fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -145,5 +145,10 @@ class UserController extends Controller
         $photo = $request->file('photo');
         $filename = $photo->getClientOriginalName();
         $photo->move(public_path(),$filename);
+       
+        $user = User::find(auth()->user()->id);
+        $user->photo = public_path().$filename;
+        $user->save();
+        return $user;
     }
 }
